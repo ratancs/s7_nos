87639b056746fff8d8e81a04391969064f45afb0
ratan <mail@ratan.co.cc>
2012-08-02 12:19:53 +0530

    simulation of copy command

---
+++ copy.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(int argc,char *argv[])
+{
+	char command[50];
+	if(argc!=3)
+		printf("ERROR : Syntax wrong try   ./copy source destination");
+	else
+	{
+		sprintf(command,"cp %s %s",argv[1],argv[2]);
+		if(system(command)==0)
+			printf("%s Sucessfully copied to %s",argv[1],argv[2]);
+		else
+			printf("Copy failed !!!");
+	}
+}
+		
+		
+	
